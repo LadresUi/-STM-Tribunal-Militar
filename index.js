bab958ede29a07d4ecbd64ea35049f5a2bcc0987
LadresUi <elitoncristyandesousa@gmail.com>
2026-04-07 02:33:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,3 +9,11 @@ client.once('ready', () => {
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN);
+{
+  "name": "stm-bot",
+  "version": "1.0.0",
+  "main": "index.js",
+  "dependencies": {
+    "discord.js": "^14.0.0"
+  }
+}
